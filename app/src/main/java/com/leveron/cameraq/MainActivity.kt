5f5868ae796cb4d22a497671e6d2afedcefc51e3
marcin <marcin@BLUESYSTEM1>
2022-10-29 00:59:36 +0200

    Wersja dzialająca z dodawanie folderów oraz spinner listą

--- app/src/main/java/com/leveron/cameraq/MainActivity.kt
+++ app/src/main/java/com/leveron/cameraq/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Debug
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -19,16 +20,28 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import com.leveron.cameraq.Constants.TAG
 import com.leveron.cameraq.R
 import com.leveron.cameraq.databinding.ActivityMainBinding
 import java.io.File
+import java.io.FileFilter
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+/*
+D ozrobienia TODO - trzeba zrobi© odswierzenie lisdty folderow na spinnerze
+TODO - trzeba wyrzuci© spinnera na ofdzielny folder
+TODO trzeba ograc przypadek gdy nie ma folderow wtedy poleci crash przy zdjeciu
+TODO  trzeba jeszcze poukladac ikonki
 
+
+
+
+
+ */
     private lateinit var binding: ActivityMainBinding
     private var  imageCapture: ImageCapture? = null
     private lateinit var outputDirectory: File
@@ -40,6 +53,10 @@ class MainActivity : AppCompatActivity() {
 //        setContentView(R.layout.activity_main)
 
         var spinner1 = binding.txtFolder
+        var btnAdd = binding.btnAddFolder
+        var txtInputFolderName = binding.txtInputFolderName
+        var listOfDirs : File = getOutputDirectory()
+        lateinit var folderSelected : String
 
         setContentView(binding.root)
         outputDirectory = getOutputDirectory()
@@ -55,6 +72,8 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
         binding.btnStrzal.setOnClickListener {
             takePhoto()
         }
@@ -63,22 +82,62 @@ class MainActivity : AppCompatActivity() {
             if (spinner1.visibility == View.VISIBLE)
             {
                 spinner1.visibility = View.INVISIBLE
+                btnAdd.visibility = View.INVISIBLE
+                txtInputFolderName.visibility = View.INVISIBLE
+
             }else{
                 spinner1.visibility = View.VISIBLE
+                btnAdd.visibility = View.VISIBLE
+                txtInputFolderName.visibility = View.VISIBLE
             }
 
 
             true
         }
 
+        binding.btnAddFolder.setOnClickListener {
+            val newFolder : File
+            //var newFolderString : String = listOfDirs.toString()
+            //newFolderString += "\" + $folderSelected
+
+            if (txtInputFolderName.length()==0){
+                Toast.makeText(this, "Folder name required", Toast.LENGTH_SHORT).show()
+            }else{
+                newFolder = File(listOfDirs.toString() + "/"+ txtInputFolderName.text)
+                Log.d("aaaa", listOfDirs.toString())
+                Log.d("aaaa", listOfDirs.toString() + "/"+ txtInputFolderName.text)
+
+                if (newFolder.exists()) {
+                    Toast.makeText(this, "Folder already exist", Toast.LENGTH_SHORT).show()
+                }else{
+                    newFolder.mkdirs()
+                    Toast.makeText(this, "Folder $txtInputFolderName created", Toast.LENGTH_SHORT).show()
+                }
 
 
-        // var spinnerList = listOf("One", "Two", "Three")
-        var spinnerList = arrayListOf<String>("One", "Two", "Three")
-        spinnerList.add("Four")
+
+            }
+
+        }
+
+
+// Tworzymy spinner adapter z lista folderow
+
+        var files : Array<File>
+        var spinnerList = arrayListOf<String>("Foldery:")
+        spinnerList.clear()
+        files = listOfDirs.listFiles(FileFilter { it.isDirectory })!!
+
+
+        Log.d("aaaa", listOfDirs.toString())
+        for (a in  files) {
+            spinnerList.add(a.name.toString())
+            Log.d("aaaa", a.name.toString())
+
+        }
+
 
         val adapter = ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, spinnerList)
-        //val adapter = ArrayAdapter.createFromResource(this, android.R.layout.simple_spinner_item, spinnerList)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
 
@@ -86,9 +145,9 @@ class MainActivity : AppCompatActivity() {
 
         spinner1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                Toast.makeText(this@MainActivity, "You selected:" + outputDirectory.toString() +
-                        "/" +  p0?.getItemAtPosition(p2).toString(),
+                Toast.makeText(this@MainActivity, "You selected:" +  p0?.getItemAtPosition(p2).toString(),
                     Toast.LENGTH_LONG).show()
+                folderSelected = p0?.getItemAtPosition(p2).toString()
 
             }
 
@@ -100,19 +159,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
- /*    val txtFolder: Spinner = binding.txtFolder
-        ArrayAdapter.createFromResource(
-            this,
-                    R.array.listOfFolders,
-                    android.R.layout.simple_spinner_dropdown_item ).also {
-                adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            txtFolder.adapter = adapter
-
-        }
-*/
 
     }
 
